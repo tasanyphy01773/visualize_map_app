ff468d1795acd190fd037c3586051c65606d75a7
Tahmidul Azom Sany <tasanyphy01773@gmail.com>
2024-05-31 01:38:50 +0600

    adsf

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -15,12 +15,21 @@ st.sidebar.image(logo)
 
 st.title("Split-panel Map")
 
+tif_path = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global_corrected.tif'
+
+# Open the GeoTIFF file
+with rasterio.open(tif_path) as src:
+    # Read the data
+    u_wind = src.read(1)  # Read the first band
+
+    v_wind = src.read(2)
+
 with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
-        before = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global_corrected.tif'
+        # before = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global_corrected.tif'
         m.split_map(
-            left_layer=before, right_layer="ESA WorldCover 2020"
+            left_layer=u_wind, right_layer=v_wind
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
