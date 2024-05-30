8fca0de080b6ef65ddc1277a4685443d78b22958
Tahmidul Azom Sany <tasanyphy01773@gmail.com>
2024-05-30 23:45:23 +0600

    new

--- pages/7_⛅_Testing_netCDF.py
+++ pages/7_⛅_Testing_netCDF.py
@@ -3,43 +3,43 @@ import netCDF4 as nc
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Title of the app
-st.title('Global U-Wind Visualization New')
+st.title('Global U-Wind Visualization')
 
-# Loading netCDF data
-@st.cache  # This decorator helps cache the data to speed up app reloads
+@st.cache
 def load_data(filepath):
-    ds = nc.Dataset(filepath)
-    u_wind = ds.variables['u_wind'][:]
-    v_wind = ds.variables['v_wind'][:]
-    lats = ds.variables['lat'][:]  # Adjust variable names as necessary
-    lons = ds.variables['lon'][:]
-    return u_wind, v_wind, lats, lons
+    try:
+        ds = nc.Dataset(filepath)
+        u_wind = ds.variables['u_wind'][:]
+        v_wind = ds.variables['v_wind'][:]
+        lats = ds.variables['lat'][:]
+        lons = ds.variables['lon'][:]
+        return u_wind, v_wind, lats, lons
+    except Exception as e:
+        st.error(f"Failed to load data: {e}")
+        return None, None, None, None
 
-# File path to your netCDF file
-file_path = 'https://raw.githubusercontent.com/tasanyphy01773/CLIM711_project/main/wind_global.nc' # I had to upload it in different folder
-u_wind, v_wind, lats, lons = load_data(file_path)
+file_path = 'https://raw.githubusercontent.com/tasanyphy01773/CLIM711_project/main/wind_global.nc'
+data = load_data(file_path)
+if data[0] is not None:
+    u_wind, v_wind, lats, lons = data
+    lon, lat = np.meshgrid(lons, lats)
 
-# Create a meshgrid for plotting
-lon, lat = np.meshgrid(lons, lats)
+    if st.checkbox('Show U-Wind Map'):
+        fig, ax = plt.subplots()
+        c = ax.pcolormesh(lon, lat, u_wind, shading='auto')
+        ax.set_xlabel('Longitude')
+        ax.set_ylabel('Latitude')
+        ax.set_title('Global U-Wind')
+        plt.colorbar(c, ax=ax, label='U-Wind (units)')
+        st.pyplot(fig)
 
-# Streamlit widget to toggle visibility
-if st.checkbox('Show U-Wind Map'):
-    fig, ax = plt.subplots()
-    c = ax.pcolormesh(lon, lat, u_wind, shading='auto')
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
-    ax.set_title('Global U-Wind')
-    plt.colorbar(c, ax=ax, label='U-Wind (units)')
-    st.pyplot(fig)
-
-
-# Streamlit widget to toggle visibility
-if st.checkbox('Show V-Wind Map'):
-    fig, ax = plt.subplots()
-    c = ax.pcolormesh(lon, lat, v_wind, shading='auto')
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
-    ax.set_title('Global V-Wind')
-    plt.colorbar(c, ax=ax, label='V-Wind (units)')
-    st.pyplot(fig)
+    if st.checkbox('Show V-Wind Map'):
+        fig, ax = plt.subplots()
+        c = ax.pcolormesh(lon, lat, v_wind, shading='auto')
+        ax.set_xlabel('Longitude')
+        ax.set_ylabel('Latitude')
+        ax.set_title('Global V-Wind')
+        plt.colorbar(c, ax=ax, label='V-Wind (units)')
+        st.pyplot(fig)
+else:
+    st.error('Unable to load and plot data due to an error with the data files.')
