6f7cfa3e609d42bd82ae7896e647520fb10e6c75
Tahmidul Azom Sany <tasanyphy01773@gmail.com>
2024-05-31 01:23:16 +0600

    adsf

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -6,6 +6,21 @@ import os
 import rasterio
 from rasterio.enums import Resampling
 from rasterio.transform import from_bounds
+import subprocess
+import sys
+
+# Function to install packages
+def install_package(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Ensure localtileserver is installed
+try:
+    import localtileserver
+except ImportError:
+    install_package('localtileserver')
+finally:
+    import localtileserver
+
 import leafmap.foliumap as leafmap
 
 st.title('Global U-Wind Visualization')
@@ -42,12 +57,6 @@ def convert_netcdf_to_geotiff(filename, tif_name, variable):
     st.write(f"Data shape: {data.shape}")
     st.write(f"Lats shape: {lats.shape}")
     st.write(f"Lons shape: {lons.shape}")
-
-    # Adjust this based on your data shape
-    if len(data.shape) == 3:
-        data = data[0, :, :]  # Assuming time is the first dimension
-    elif len(data.shape) != 2:
-        st.error("Unexpected data shape")
 
     transform = from_bounds(lons.min(), lats.min(), lons.max(), lats.max(), data.shape[1], data.shape[0])
 
@@ -97,88 +106,3 @@ with col1:
     m.add_raster(corrected_tif_path, palette="coolwarm", layer_name="u_wind")
     m.add_basemap(basemap)
     m.to_streamlit(height=700)
-
-
-# import streamlit as st
-# import netCDF4 as nc
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import requests
-# import os
-
-# st.title('Global U-Wind Visualization')
-
-# def download_file(url, filename):
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         with open(filename, 'wb') as f:
-#             f.write(response.content)
-#         return True
-#     else:
-#         return False
-
-# @st.cache
-# def load_data(filepath):
-#     try:
-#         ds = nc.Dataset(filepath)
-#         u_wind = ds.variables['u_wind'][:]
-#         v_wind = ds.variables['v_wind'][:]
-#         lats = ds.variables['lat'][:]
-#         lons = ds.variables['lon'][:]
-#         return u_wind, v_wind, lats, lons
-#     except Exception as e:
-#         st.error(f"Failed to load data: {e}")
-#         return None, None, None, None
-
-# # URL of the .nc file
-# url = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global.nc'
-# filename = 'wind_global.nc'
-
-# # Check if file is not already downloaded
-# if not os.path.exists(filename):
-#     result = download_file(url, filename)
-#     if not result:
-#         st.error('Failed to download file. Please check the URL or network settings.')
-
-# # data = load_data(filename)
-
-# import leafmap.foliumap as leafmap
-
-# markdown = """
-# A Streamlit map template
-# <https://github.com/opengeos/streamlit-map-template>
-# """
-
-# st.sidebar.title("About")
-# st.sidebar.info(markdown)
-# logo = "https://i.imgur.com/UbOXYAU.png"
-# st.sidebar.image(logo)
-
-
-# st.title("Interactive Map")
-
-# col1, col2 = st.columns([4, 1])
-# options = list(leafmap.basemaps.keys())
-# index = options.index("OpenTopoMap")
-
-# with col2:
-
-#     basemap = st.selectbox("Select a basemap:", options, index)
-
-
-# with col1:
-
-#     m = leafmap.Map(
-#         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-#     )
-#     m.add_netcdf(
-#         filename,
-#         variables=["u_wind"],
-#         palette="coolwarm",
-#         shift_lon=True,
-#         layer_name="u_wind",
-#         indexes=[1]  # Ensure that this index is applicable for your data
-#     )
-
-#     m.add_basemap(basemap)
-#     m.to_streamlit(height=700)
