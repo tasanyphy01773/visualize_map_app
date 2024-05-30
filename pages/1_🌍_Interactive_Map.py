db6965478a95221162adacaa8a259b7690201db2
Tahmidul Azom Sany <tasanyphy01773@gmail.com>
2024-05-31 01:13:20 +0600

    sfdg

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -1,40 +1,83 @@
 import streamlit as st
-import rasterio
-from rasterio.enums import Resampling
-from leafmap.leafmap import netcdf_to_tif
-import folium
-from streamlit_folium import folium_static
+import netCDF4 as nc
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import os
 
 st.title('Global U-Wind Visualization')
 
-# Function to convert NetCDF to GeoTIFF
-def convert_netcdf_to_geotiff(filename, tif_name):
-    netcdf_to_tif(filename, tif_name, variables=["u_wind"], shift_lon=True)
-    with rasterio.open(tif_name) as src:
-        profile = src.profile
-        profile.update({'crs': 'epsg:4326'})
-        corrected_tif_path = 'wind_global_corrected.tif'
-        with rasterio.open(corrected_tif_path, 'w', **profile) as dst:
-            for i in range(1, src.count + 1):
-                data = src.read(i, resampling=Resampling.nearest)
-                dst.write(data, i)
-    return corrected_tif_path
+def download_file(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        return True
+    else:
+        return False
 
-# Download and prepare data
-filename = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global.nc'
-tif_name = "wind_global.tif"
-corrected_tif_path = convert_netcdf_to_geotiff(filename, tif_name)
+@st.cache
+def load_data(filepath):
+    try:
+        ds = nc.Dataset(filepath)
+        u_wind = ds.variables['u_wind'][:]
+        v_wind = ds.variables['v_wind'][:]
+        lats = ds.variables['lat'][:]
+        lons = ds.variables['lon'][:]
+        return u_wind, v_wind, lats, lons
+    except Exception as e:
+        st.error(f"Failed to load data: {e}")
+        return None, None, None, None
 
-if corrected_tif_path:
-    # Create a Folium map
-    m = folium.Map(location=[0, 0], zoom_start=2)
-    folium.raster_layers.TileLayer(tiles=corrected_tif_path, attr='U-Wind').add_to(m)
-    # Optionally add geojson for countries
-    geojson_url = "https://github.com/opengeos/leafmap/raw/master/examples/data/countries.geojson"
-    folium.GeoJson(geojson_url, name="Countries").add_to(m)
-    folium.LayerControl().add_to(m)
-    
-    # Display map in Streamlit
-    folium_static(m)
-else:
-    st.error("Failed to generate the map due to an error in the file conversion.")
+# URL of the .nc file
+url = 'https://github.com/tasanyphy01773/visualize_map_app/releases/download/dataset/wind_global.nc'
+filename = 'wind_global.nc'
+
+# Check if file is not already downloaded
+if not os.path.exists(filename):
+    result = download_file(url, filename)
+    if not result:
+        st.error('Failed to download file. Please check the URL or network settings.')
+
+# data = load_data(filename)
+
+import leafmap.foliumap as leafmap
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+
+st.title("Interactive Map")
+
+col1, col2 = st.columns([4, 1])
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+    )
+    m.add_netcdf(
+        filename,
+        variables=["u_wind"],
+        palette="coolwarm",
+        shift_lon=True,
+        layer_name="u_wind",
+        indexes=[1]  # Ensure that this index is applicable for your data
+    )
+
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
